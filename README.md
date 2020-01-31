346af332ea2ba1c27b86c4229a2b0f6e2868c38b
Bohdan <bohdanimiela@gmail.com>
2020-01-31 19:19:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Website build from this design:
+[Productly landing page by Abdullah Un Noman](https://dribbble.com/shots/9558318-Free-Free-Productly-landing-page)
---
+++ webpack.common.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif|ico)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'imgs'
+          }
+        }
+      },
+      {
+        test: /\.(ttf|eof|woff|woff2)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'fonts'
+          }
+        }
+      }
+    ]
+  }
+}
